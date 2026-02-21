43e0431dd41d0bb82857dc910d3bf3f4d05c9e3d
SFMand <sultanfbm12@gmail.com>
2026-02-21 03:43:03 +0300

    refactor: format clearer analysis, limit testing

--- main.py
+++ main.py
@@ -5,18 +5,25 @@ import logic.brute_force as bf
 test_cases = [
         ("Shortest Random DNAs", dna.shortestDna),
         ("Short Random DNAs", dna.shortDna),
-        ("Long Random DNAs", dna.longDna),
-        ("Longer Random DNAs", dna.longerDna),
-        ("Longest Random DNAs", dna.longestDna)
+        # ("Long Random DNAs", dna.longDna),
+        # ("Longer Random DNAs", dna.longerDna),
+        # ("Longest Random DNAs", dna.longestDna)
     ]
 
 for case_name, dna_rand in test_cases:
         print(f"\n--- {case_name} ---")
         dna1 = dna_rand()
         dna2 = dna_rand() 
-        start_time = time.time()
-        print(f"Analysis start time: {start_time}\nDNA1: {dna1[:400]}\nDNA2: {dna2[:400]}")
-        bf.brute_force(dna1, dna2)
-        end_time = time.time()
+        start_time = time.perf_counter()
+        print(f"Analysis start time: {start_time}\nDNA1: {dna1}\nDNA2: {dna2}")
+        best_score, aligned1, aligned2 = bf.brute_force(dna1, dna2)
+        end_time = time.perf_counter()
         print(f"Analysis end time: {end_time}")
-        print(f"Time taken: {end_time - start_time} seconds")
+        
+        print(f"""
+              Best alignment score: {best_score}
+              Aligned DNA strings:
+              {aligned1}
+              {aligned2}
+              Time taken: {(end_time - start_time) * 1000: .3f} milliseconds
+              """)
