42bc2d35baf394fe25c84da7b9db4b4bb6732339
SFMand <sultanfbm12@gmail.com>
2026-02-21 03:16:26 +0300

    chore: formulate strings depending on pos1,2 combinations

--- logic/brute_force.py
+++ logic/brute_force.py
@@ -13,5 +13,20 @@ def brute_force(dna1, dna2):
 	for align_length in range(max(len1, len2), len1 + len2 + 1):
 		for pos1 in combinations(range(align_length), len1):
 			for pos2 in combinations(range(align_length), len2):
-				print(f"Position 1: {pos1}\tPosition 2: {pos2}")
+				a1_chars, a2_chars = [], []
+				ptr1, ptr2 = 0, 0
+				for idx in range(align_length):
+					if ptr1 < len1 and pos1[ptr1] == idx:
+						char1 = dna1[ptr1]
+						ptr1 += 1
+					else:
+						char1 = '-'
+					if ptr2 < len2 and pos2[ptr2] == idx:
+						char2 = dna2[ptr2]
+						ptr2 += 1
+					else:
+						char2 = '-'
+					a1_chars.append(char1)
+					a2_chars.append(char2)
+				print(f"String 1 based on pos1: {"".join(a1_chars)}\tString 2 based on pos2: {"".join(a2_chars)}")
 	return best_score, best_a1, best_a2
