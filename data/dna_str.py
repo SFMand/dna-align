0fa550cdcb5ccf9430be6805ae0e87e63aaf9730
SFMand <sultanfbm12@gmail.com>
2026-02-21 03:44:34 +0300

    fix: lower input size

--- data/dna_str.py
+++ data/dna_str.py
@@ -4,13 +4,13 @@ def shortestDna():
     return ''.join(random.choices(['A', 'C', 'G', 'T'], k=4))
 
 def shortDna():
-    return ''.join(random.choices(['A', 'C', 'G', 'T'], k=40))
+    return ''.join(random.choices(['A', 'C', 'G', 'T'], k=8))
 
 def longDna():
-    return ''.join(random.choices(['A', 'C', 'G', 'T'], k=4000))
+    return ''.join(random.choices(['A', 'C', 'G', 'T'], k=40))
 
 def longerDna():
-    return ''.join(random.choices(['A', 'C', 'G', 'T'], k=40000))
+    return ''.join(random.choices(['A', 'C', 'G', 'T'], k=100))
 
 def longestDna():
-    return ''.join(random.choices(['A', 'C', 'G', 'T'], k=400000))  
+    return ''.join(random.choices(['A', 'C', 'G', 'T'], k=400))  
